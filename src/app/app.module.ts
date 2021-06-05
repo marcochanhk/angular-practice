59ddc66ba3b8dc73f9014cd962c9a8aa31a4da54
marcochanhk <mc_marcochan@outlook.com>
2021-06-06 02:15:31 +0800

    Revert "Revert "20210606""
    This reverts commit 491f12eb4372019149c7893139848b00d74fb160.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
